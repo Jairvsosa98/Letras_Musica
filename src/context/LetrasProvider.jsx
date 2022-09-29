345399f9aac95b9ded85807637ceef8ee91f90e4
Jairvsosa98 <jairvsosa@gmail.com>
2022-09-29 14:32:17 -0500

    first commit

---
+++ src/context/LetrasProvider.jsx
@@ -0,0 +1,49 @@
+import { useState, createContext } from "react";
+import axios from "axios";
+const LetrasContext = createContext()
+
+const LetrasProvider = ({children}) => {
+
+    const [alerta, setAlerta] = useState('')
+    const [letra, setLetra] = useState('')
+    const [cargando, setCargando] = useState(false)
+    const busquedaLetra = async (busqueda) => {
+        setCargando(true)
+        try {
+            const {artista, cancion} = busqueda
+            const options = {
+                method: 'GET',
+                headers: {
+                    'X-RapidAPI-Key': `${import.meta.env.VITE_API_KEY}`,
+                    'X-RapidAPI-Host': `${import.meta.env.VITE_API_HOST}`
+                }
+            };
+
+            const url = `${import.meta.env.VITE_API_URL}/${artista}/${cancion}`
+            const {data} = await axios(url,options)
+            setLetra(data.lyrics)
+            setAlerta('')
+        } catch (error) {
+            setAlerta('Canción no Encontrada')
+            console.log(error)
+        }
+        setCargando(false)
+    }
+
+    return (
+        <LetrasContext.Provider
+        value={{
+            alerta,
+            setAlerta,
+            busquedaLetra,
+            letra,
+            cargando
+        }}>
+            {children}
+        </LetrasContext.Provider>
+    )
+}
+export {
+    LetrasProvider
+}
+export default LetrasContext
